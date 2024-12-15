1f2466df15647f39ad206d4a22cd2ab6660b2b1a
Cooper D'Andilly <151992386+CooperDActor-bytes@users.noreply.github.com>
2024-12-15 15:58:34 +1100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@ const apiKey = 'AIzaSyBKC-k_7uhy35oVrr52fzWRzpgvL2QBnys'; // Replace with your a
 const channelId = 'UC8ObOjIpTEwLrzlssxBAQEw';   // Replace with your actual YouTube channel ID
 
 // YouTube API URL for fetching videos (uploads playlist)
-const videoUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId=UU$UC8ObOjIpTEwLrzlssxBAQEw&key=$AIzaSyBKC-k_7uhy35oVrr52fzWRzpgvL2QBnys`;
+const videoUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId=UUUC8ObOjIpTEwLrzlssxBAQEw&key=$IzaSyBKC-k_7uhy35oVrr52fzWRzpgvL2QBnys`;
 // YouTube API URL for fetching shorts
-const shortsUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&channelId=$UC8ObOjIpTEwLrzlssxBAQEw&videoDuration=short&key=$AIzaSyBKC-k_7uhy35oVrr52fzWRzpgvL2QBnys`;
+const shortsUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&channelId=UC8ObOjIpTEwLrzlssxBAQEw&videoDuration=short&key=AIzaSyBKC-k_7uhy35oVrr52fzWRzpgvL2QBnys`;
 
 // Fetch and display the latest videos
 fetch(videoUrl)
